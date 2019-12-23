20b5dba1396741e128d23f71ff96c7082cfd2bc5
lichengbo <lichengbo@huishoubao.com.cn>
2019-12-23 16:46:50 +0800

    add blueprints

--- application/controller/testApi.py
+++ application/controller/testApi.py
@@ -1,11 +1,10 @@
 from flask import request
 from flask import blueprints
 
-from application import app
 from application.middles import testMiddle as m
 from application.controller import route
 
-bp = blueprints.Blueprint(app, __name__, url_prefix='/example')
+bp = blueprints.Blueprint('test', __name__, url_prefix='/example')
 
 
 @route(bp, '/get_test_info', methods=['POST'])
