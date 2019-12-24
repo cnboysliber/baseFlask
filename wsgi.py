bc090bb413749e02125d02c7433f6d07902a4afa
lichengbo <lichengbo@huishoubao.com.cn>
2019-12-24 17:35:18 +0800

    add gunicorn

--- wsgi.py
+++ wsgi.py
@@ -6,11 +6,10 @@ if _basedir not in sys.path:
     sys.path.insert(0, _basedir)
 
 from werkzeug.serving import run_simple
-from application.config import default_config as cfg
 from application import app
 from application.controller import register_blueprints
 
 register_blueprints(app)
 
 if __name__ == '__main__':
-    run_simple(cfg.HOST, cfg.PORT, app)
+    run_simple(app.config['HOST'], app.config['PORT'], app)
