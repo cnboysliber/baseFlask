1fba3327a20f2be306e9452bc396731db4d3aa98
lichengbo <lichengbo@huishoubao.com.cn>
2019-12-24 17:10:57 +0800

    add gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 from werkzeug.serving import run_simple
+from application.config import default_config as cfg
 from application import app
 from application.controller import register_blueprints
 
 register_blueprints(app)
 
 if __name__ == '__main__':
-    run_simple(hostname=app.config['HOST'], port=app.config['PORT'], application=app)
+    run_simple(hostname=cfg.HOST, port=cfg.PORT, application=app)
