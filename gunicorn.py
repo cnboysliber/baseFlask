d28466dbc70b2013a0e23440d79a9c4ad92be76b
lichengbo <lichengbo@huishoubao.com.cn>
2019-12-24 17:14:36 +0800

    add gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,4 +1,10 @@
 # -*- coding:utf-8 -*-
+from __future__ import absolute_import
+import os
+import sys
+_basedir = os.path.abspath(os.path.dirname(__file__))
+if _basedir not in sys.path:
+    sys.path.insert(0, _basedir)
 # from application.config.default_config import HOST, PORT
 bind = "{ip}:{port}".format(ip='0.0.0.0', port=9091)
 backlog = 2048
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
+from __future__ import absolute_import
+import os
+import sys
+_basedir = os.path.abspath(os.path.dirname(__file__))
+if _basedir not in sys.path:
+    sys.path.insert(0, _basedir)
+
 from werkzeug.serving import run_simple
 from application.config import default_config as cfg
 from application import app
